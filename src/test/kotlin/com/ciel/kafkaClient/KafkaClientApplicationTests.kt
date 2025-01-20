3ff91712c66edd1216d564617aec96b2db9f826f
ben <Lim6112j@gmail.com>
2025-01-20 15:13:08 +0900

    test

--- src/test/kotlin/com/ciel/kafkaClient/KafkaClientApplicationTests.kt
+++ src/test/kotlin/com/ciel/kafkaClient/KafkaClientApplicationTests.kt
@@ -3,11 +3,17 @@ package com.ciel.kafkaClient
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.test.utils.KafkaTestUtils
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.test.EmbeddedKafkaBroker
 
+@EmbeddedKafka
 @SpringBootTest
 class KafkaClientApplicationTests {
-
 	@Test
-	fun contextLoads() {
+	fun someTest(broker : EmbeddedKafkaBroker) {
+		// val embeddedKafka = EmbeddedKafkaRule(3, true, "topic")
+		// val consumerProps = KafkaTestUtils.consumerProps("ciel", "true", broker)
+		// val producerProps = KafkaTestUtils.producerProps(broker)
 	}
 }
